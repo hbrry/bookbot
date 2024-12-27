119a979d9be22e350284e13ee20300980a832536
barry <barryhamzata@gmail.com>
2024-12-27 14:35:18 +0100

    Cleaning out report

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 def main():
     with open("/home/barry/workspace/github.com/hbbry/bookbot/books/frankenstein.txt") as f:
         text = f.read()
-        words = text.split()
-    return print(count_characters(text))
+    
+    print(f"--- Begin report of {"/home/barry/workspace/github.com/hbbry/bookbot/books/frankenstein.txt"} ---")
+    print(f"{get_num_words(text)} words found in the document")
+    print()
+
+    for item in char_to_list(count_characters(text)):
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+
+def sort_on(d):
+    return d["num"]
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 def count_characters(text):
     lowered_text = text.lower()
@@ -14,6 +32,13 @@ def count_characters(text):
             char_dict[char] = 1
 
     return char_dict
+
+def char_to_list(dict):
+    sorted = []
+    for char in dict:
+        sorted.append({"char": char, "num":dict[char]})
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
 
 if __name__ == "__main__":
     main()
