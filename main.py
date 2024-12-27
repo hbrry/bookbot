3481eeddfebd7e1fe6212e2bed95d87eb9ee56e2
barry <barryhamzata@gmail.com>
2024-12-27 14:26:55 +0100

    count characters

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ def main():
     with open("/home/barry/workspace/github.com/hbbry/bookbot/books/frankenstein.txt") as f:
         text = f.read()
         words = text.split()
-    return print(len(words))
+    return print(count_characters(text))
 
 def count_characters(text):
     lowered_text = text.lower()
+    char_dict = {}
+    for char in lowered_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
 
+    return char_dict
 
 if __name__ == "__main__":
     main()
